15749533b6e4c6fbda4d91b91678c3b55ed8d38e
Ronald <Ronald@Ronald.com>
2025-12-11 12:18:10 +1100

    Fix Missing CommunityToolkit.Maui and IconTintColorBehavior

--- MauiScraperApp/MauiProgram.cs
+++ MauiScraperApp/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using MauiScraperApp.Services;
+﻿using CommunityToolkit.Maui;
+using MauiScraperApp.Services;
 using MauiScraperApp.ViewModels;
 using MauiScraperApp.Views;
 using System.Net;
@@ -14,6 +15,7 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
